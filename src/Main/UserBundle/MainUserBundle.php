a4f8a1fbe0620a549e23f5af66355f5fbfc95c1d
hagar72 <hagar.as3ad@gmail.com>
2015-05-13 22:25:11 +0200

    update user bundle

--- src/Main/UserBundle/MainUserBundle.php
+++ src/Main/UserBundle/MainUserBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MainUserBundle extends Bundle
 {
-	public function getParent()
+    public function getParent()
     {
         return 'FOSUserBundle';
     }

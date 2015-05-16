dfaff7a493b6f9af9b0d38eb142259fdc646a20c
hagar72 <hagar.as3ad@gmail.com>
2015-05-16 21:47:40 +0200

    remove dummy code

--- src/Main/AbstractBundle/EventListener/LocaleListener.php
+++ src/Main/AbstractBundle/EventListener/LocaleListener.php
@@ -21,7 +21,6 @@ class LocaleListener implements EventSubscriberInterface
             return;
         }
 
-        file_put_contents('hagar', $request->attributes->get('_locale'));
         // try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->attributes->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
